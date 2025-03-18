0ae9c5447103e0f4b95281cd6fddd7733a9f65ee
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-18 21:59:12 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
