35be8e677007e35917b36be2dda9c23f40d9ab64
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-18 22:03:35 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,15 @@ export default function RootLayout({ children }) {
         >
           {/* header */}
           <main className="min-h-screen">{children}</main>
+
           {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>
+                Made with ❤ by Kanishka.
+              </p>
+            </div>
+          </footer>
           
         </ThemeProvider>
       </body>
