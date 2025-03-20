49f72b5428e92fa44484baa55a22d7f0b0808266
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-20 16:19:23 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -10,43 +10,53 @@ const Hero = () => {
     const imageRef = useRef(null);
 
     useEffect(() => {
-        const scrollPosition = window.scrollY;
-        const scrollThreshold = 100;
-    },[])
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+
+            if (scrollPosition > scrollThreshold) {
+                imageElement.classList.add("scrolled");
+            }
+
+        }
+
+    }, [])
 
 
-  return (
-    <section className="w-full pt-36 md:pt-48 pb-10">
-        <div className="space-y-6 text-center">
-            <div className="space-y-6 mx-auto">
-                <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">Your AI Career Coach for <br /> Professional Success</h1>
-                <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">Advance your career with personalised guidance, interview preparartion, and AI-powered tools for job success.</p>
-            </div>
+    return (
+        <section className="w-full pt-36 md:pt-48 pb-10">
+            <div className="space-y-6 text-center">
+                <div className="space-y-6 mx-auto">
+                    <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">Your AI Career Coach for <br /> Professional Success</h1>
+                    <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">Advance your career with personalised guidance, interview preparartion, and AI-powered tools for job success.</p>
+                </div>
 
-            <div className="flex justify-center space-x-4">
-                <Link href='/dashboard'>
-                    <Button size="lg" className="px-8">Get Started</Button>
-                </Link>
-                <Link href='https://www.youtube.com/kanishkadz'>
-                    <Button size="lg" className="px-8" variant="outline">Get Started</Button>
-                </Link>
-            </div>
+                <div className="flex justify-center space-x-4">
+                    <Link href='/dashboard'>
+                        <Button size="lg" className="px-8">Get Started</Button>
+                    </Link>
+                    <Link href='https://www.youtube.com/kanishkadz'>
+                        <Button size="lg" className="px-8" variant="outline">Get Started</Button>
+                    </Link>
+                </div>
 
-            <div className="hero-image-wrapper mt-5 md:mt-0">
-                <div ref={imageRef} className="hero-image">
-                    <Image 
-                    src={"/banner.jpeg"} 
-                    width={1280} 
-                    height={720} 
-                    alt="Banner Elevate" 
-                    className="rounded-lg shadow-2xl border mx-auto" 
-                    priority
-                    />
+                <div className="hero-image-wrapper mt-5 md:mt-0">
+                    <div ref={imageRef} className="hero-image">
+                        <Image
+                            src={"/banner.jpeg"}
+                            width={1280}
+                            height={720}
+                            alt="Banner Elevate"
+                            className="rounded-lg shadow-2xl border mx-auto"
+                            priority
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Hero
