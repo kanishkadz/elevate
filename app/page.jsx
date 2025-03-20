6c347493b33cf587363e700b7f0f4cb2f0f3a068
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-20 16:12:53 +0530

    2

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,8 @@ import HeroSection from "@/components/hero";
 export default function Home() {
   return (
     <div>
-      <div className="grid-background">
-
-        <HeroSection />
-
-      </div>
+      <div className="grid-background"></div>
+      <HeroSection />
     </div>
   );
 }
