74da2d7c235f917368c9bd45fba149c9baf2b170
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-20 16:09:43 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const Hero = () => {
+
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const scrollPosition = window.scrollY;
+        const scrollThreshold = 100;
+    },[])
+
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
         <div className="space-y-6 text-center">
             <div className="space-y-6 mx-auto">
                 <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">Your AI Career Coach for <br /> Professional Success</h1>
-                <p>Advance your career with personalised guidance, interview preparartion, and AI-powered tools for job success.</p>
+                <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">Advance your career with personalised guidance, interview preparartion, and AI-powered tools for job success.</p>
             </div>
 
-            <div>
+            <div className="flex justify-center space-x-4">
                 <Link href='/dashboard'>
                     <Button size="lg" className="px-8">Get Started</Button>
                 </Link>
@@ -20,8 +31,9 @@ const Hero = () => {
                     <Button size="lg" className="px-8" variant="outline">Get Started</Button>
                 </Link>
             </div>
-            <div>
-                <div>
+
+            <div className="hero-image-wrapper mt-5 md:mt-0">
+                <div ref={imageRef} className="hero-image">
                     <Image 
                     src={"/banner.jpeg"} 
                     width={1280} 
