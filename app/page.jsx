3a7f10777d42419b4596475569b4557cf27d19ff
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-20 16:26:10 +0530

    2

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,12 @@ export default function Home() {
     <div>
       <div className="grid-background"></div>
       <HeroSection />
+
+      <section>
+        <div>
+          <h2>Powerful Features for Your Career Growth</h2>
+        </div>
+      </section>
     </div>
   );
 }
