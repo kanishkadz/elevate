4201d03508a7668b7de67dbe56a94e5658c13c58
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-20 17:12:48 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -85,7 +85,7 @@ export default function Home() {
 
       <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
         <div className="container mx-auto px-4 md:px-6">
-          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">What our User say ?</h2>
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">What our Users say ?</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
             {testimonial.map((testimonal, index) => {
               return (
