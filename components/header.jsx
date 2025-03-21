98f085a76bc5659677fb240b8a18d79f1114f8a9
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-21 22:22:31 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -5,9 +5,12 @@ import Image from 'next/image';
 import { Button } from './ui/button';
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { checkUser } from '@/lib/checkUser';
+
+const Header = async () => {
+    await checkUser();
 
 
-const Header = () => {
     return (
         <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
             <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
